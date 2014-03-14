da68e9a2eae98f5eb6c4aa121a53bc6ac32be4f0
JamieHong <jamiehongbydesign@gmail.com>
2014-03-14 13:52:01 -0400

    Initial Commit

---
+++ CatNap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CatNap
+//
+//  Created by Jamie Hong on 3/14/14.
+//  Copyright (c) 2014 JamieHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
